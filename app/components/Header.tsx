68a4c8832eeedcaaf55c9c0767516915541ff2c5
eek22555388 <22555388@students.latrobe.edu.au>
2026-07-26 12:32:58 +1000

    posts updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-slate-800 text-white p-4 dark:bg-slate-950">
       <h1 className="text-xl font-bold">
-        CSE5006 Assessment 1 — RSS Server Frontend
+        CSE5006 Assessment - my blog
       </h1>
     </header>
   );
